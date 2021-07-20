bbe2f92fb82d2d9022888f0345aee2b74946905d
Piotr Orzechowski <piotr.orzechowski@kit.edu>
2021-07-20 13:08:22 +0200

    Define Duration in seconds

--- include/behavior_planning/types.hpp
+++ include/behavior_planning/types.hpp
@@ -5,7 +5,9 @@
 
 namespace behavior_planning {
 
-using Time = Clock::time_point;
 using Clock = std::chrono::steady_clock;
+//! duration in seconds as double
+using Duration = std::chrono::duration<double, std::ratio<1>>;
+using Time = std::chrono::time_point<Clock, Duration>;
 
 } // namespace behavior_planning
