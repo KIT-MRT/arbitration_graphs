13d687bca9c24d838563f5b48fb3c2091e8b9b9e
Nick Le Large <nick.lelarge@kit.edu>
2024-04-08 13:55:05 +0200

    Add random arbitrator and corresponding unit test

---
+++ include/arbitration_graphs/internal/random_arbitrator_io.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "../random_arbitrator.hpp"
+
+
+namespace arbitration_graphs {
+
+
+//////////////////////////////////////
+//    RandomArbitrator::Option    //
+//////////////////////////////////////
+
+template <typename CommandT, typename SubCommandT, typename VerifierT, typename VerificationResultT>
+std::ostream& RandomArbitrator<CommandT, SubCommandT, VerifierT, VerificationResultT>::Option::to_stream(
+    std::ostream& output,
+    const Time& time,
+    const int& option_index,
+    const std::string& prefix,
+    const std::string& suffix) const {
+
+    output << option_index + 1 << ". ";
+    ArbitratorBase::Option::to_stream(output, time, option_index, prefix, suffix);
+    return output;
+}
+
+
+//////////////////////////////////////
+//        RandomArbitrator        //
+//////////////////////////////////////
+
+template <typename CommandT, typename SubCommandT, typename VerifierT, typename VerificationResultT>
+YAML::Node RandomArbitrator<CommandT, SubCommandT, VerifierT, VerificationResultT>::toYaml(const Time& time) const {
+    YAML::Node node = ArbitratorBase::toYaml(time);
+    node["type"] = "RandomArbitrator";
+    return node;
+}
+
+} // namespace arbitration_graphs
---
+++ include/arbitration_graphs/random_arbitrator.hpp
@@ -0,0 +1,115 @@
+#pragma once
+
+#include <memory>
+#include <random>
+
+#include <yaml-cpp/yaml.h>
+
+#include "arbitrator.hpp"
+
+namespace arbitration_graphs {
+
+template <typename CommandT,
+          typename SubCommandT = CommandT,
+          typename VerifierT = verification::PlaceboVerifier<SubCommandT>,
+          typename VerificationResultT = typename decltype(std::function{VerifierT::analyze})::result_type>
+class RandomArbitrator : public Arbitrator<CommandT, SubCommandT, VerifierT, VerificationResultT> {
+public:
+    using ArbitratorBase = Arbitrator<CommandT, SubCommandT, VerifierT, VerificationResultT>;
+
+    using Ptr = std::shared_ptr<RandomArbitrator>;
+    using ConstPtr = std::shared_ptr<const RandomArbitrator>;
+
+    struct Option : public ArbitratorBase::Option {
+    public:
+        using Ptr = std::shared_ptr<Option>;
+        using FlagsT = typename ArbitratorBase::Option::FlagsT;
+        using ConstPtr = std::shared_ptr<const Option>;
+
+        enum Flags { NO_FLAGS = 0b0, INTERRUPTABLE = 0b1, FALLBACK = 0b10 };
+
+        Option(const typename Behavior<SubCommandT>::Ptr& behavior, const FlagsT& flags, const double& weight)
+                : ArbitratorBase::Option(behavior, flags), weight_(weight) {
+        }
+
+        /*!
+         * \brief Writes a string representation of the behavior option and its
+         * current state to the output stream.
+         *
+         * \param output        Output stream to write into, will be returned also
+         * \param time          Expected execution time point of this behaviors command
+         * \param option_index  Position index of this option within behaviorOptions_
+         * \param prefix        A string that should be prepended to each line that is written to the output stream
+         * \param suffix        A string that should be appended to each line that is written to the output stream
+         * \return              The same given input stream (signature similar to std::ostream& operator<<())
+         *
+         * \see Arbitrator::to_stream()
+         */
+        virtual std::ostream& to_stream(std::ostream& output,
+                                        const Time& time,
+                                        const int& option_index,
+                                        const std::string& prefix = "",
+                                        const std::string& suffix = "") const;
+
+        double weight_;
+    };
+    using Options = std::vector<typename Option::Ptr>;
+
+    RandomArbitrator(const std::string& name = "RandomArbitrator", const VerifierT& verifier = VerifierT())
+            : ArbitratorBase(name, verifier){};
+
+    void addOption(const typename Behavior<SubCommandT>::Ptr& behavior,
+                   const typename Option::FlagsT& flags,
+                   const double& weight = 1) {
+        typename Option::Ptr option = std::make_shared<Option>(behavior, flags, weight);
+        this->behaviorOptions_.push_back(option);
+    }
+
+    /*!
+     * \brief Returns a yaml representation of the arbitrator object with its current state
+     *
+     * \param time  Expected execution time point of this behaviors command
+     * \return      Yaml representation of this behavior
+     */
+    virtual YAML::Node toYaml(const Time& time) const override;
+
+protected:
+    /*!
+     * @brief   Sort behavior options randomly considering their respective weights
+     *
+     * @return  Behavior options sorted randomly considering their respective weights
+     */
+    typename ArbitratorBase::Options sortOptionsByGivenPolicy(const typename ArbitratorBase::Options& options,
+                                                              const Time& time) const override {
+        return weightedShuffle(options);
+    }
+
+    static typename ArbitratorBase::Options weightedShuffle(const typename ArbitratorBase::Options& options) {
+        typename ArbitratorBase::Options shuffledOptions;
+        shuffledOptions.reserve(options.size());
+
+        std::vector<double> weights;
+        weights.reserve(options.size());
+        for (const auto& optionBase : options) {
+            typename Option::Ptr option = std::dynamic_pointer_cast<Option>(optionBase);
+            weights.push_back(option->weight_);
+        }
+
+        std::random_device randomDevice;
+        std::mt19937 randomGenerator(randomDevice());
+
+        while (!weights.empty()) {
+            std::discrete_distribution<int> discreteDistribution(weights.begin(), weights.end());
+            auto index = discreteDistribution(randomGenerator);
+
+            shuffledOptions.push_back(options.at(index));
+            weights.erase(weights.begin() + index);
+        }
+
+        return shuffledOptions;
+    }
+
+}; // namespace arbitration_graphs
+} // namespace arbitration_graphs
+
+#include "internal/random_arbitrator_io.hpp"
