6e6222eef70122cceb6dc67881c9ff36848dc0b4
Piotr Spieker <orzechow@posteo.de>
2024-04-12 10:52:53 +0200

    Draft pacman demo

---
+++ demo/main.cpp
@@ -0,0 +1,20 @@
+#include <cstdlib>
+
+
+int main() {
+  // Initialization Game Engine
+
+  // Initialization Behaviors / Arbitration Graph
+
+  // Loop
+
+  //// Get environment state from game engine
+  //// Update environment model
+
+  //// Get command for ego player \todo find his name
+
+  //// Send command to game engine
+  //// Update game state
+
+  return EXIT_SUCCESS;
+}
