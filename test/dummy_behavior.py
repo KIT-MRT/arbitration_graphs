84a4bc9e8b94ceac4dfe309d9bdcea281906416f
Nick Le Large <nick.lelarge@kit.edu>
2024-10-10 13:07:35 +0200

    Re-implement DummyBehavior in python and run equivalent unit test

--- test/dummy_behavior.py
+++ test/dummy_behavior.py
@@ -8,7 +8,11 @@ import time
 import unittest
 
 import yaml
+
 import arbitration_graphs_py as ag
+from dummy_types import DummyBehaviorPy, DummyCommandPy, PrintStrings
+
+
 class DummyBehaviorTest(unittest.TestCase):
 
     def setUp(self):
@@ -50,6 +54,52 @@ class DummyBehaviorTest(unittest.TestCase):
         self.assertEqual("dummy_behavior", yaml_node["name"])
         self.assertTrue(yaml_node["invocationCondition"])
         self.assertTrue(yaml_node["commitmentCondition"])
+
+
+class DummyBehaviorPyTest(unittest.TestCase):
+
+    def setUp(self):
+        self.test_behavior_true = DummyBehaviorPy(True, True)
+
+        self.expected_command = DummyCommandPy("dummy_behavior")
+
+        self.time = time.time()
+
+    def test_basic_interface(self):
+        self.assertEqual(
+            self.expected_command, self.test_behavior_true.get_command(self.time)
+        )
+        self.assertTrue(self.test_behavior_true.check_commitment_condition(self.time))
+        self.assertTrue(self.test_behavior_true.check_invocation_condition(self.time))
+
+        try:
+            self.test_behavior_true.gain_control(self.time)
+        except Exception as e:
+            self.fail(f"gain_control threw an exception: {e}")
+        try:
+            self.test_behavior_true.lose_control(self.time)
+        except Exception as e:
+            self.fail(f"lose_control threw an exception: {e}")
+
+    def test_printout(self):
+        expected_printout = (
+            PrintStrings.invocation_true_string
+            + PrintStrings.commitment_true_string
+            + "dummy_behavior"
+        )
+        actual_printout = self.test_behavior_true.to_str(self.time)
+        print(actual_printout)
+
+        self.assertEqual(expected_printout, actual_printout)
+
+    def test_to_yaml(self):
+        yaml_node = yaml.safe_load(self.test_behavior_true.to_yaml_as_str(self.time))
+
+        self.assertEqual("Behavior", yaml_node["type"])
+        self.assertEqual("dummy_behavior", yaml_node["name"])
+        self.assertTrue(yaml_node["invocationCondition"])
+        self.assertTrue(yaml_node["commitmentCondition"])
+
 
 if __name__ == "__main__":
     unittest.main()
---
+++ test/dummy_types.py
@@ -0,0 +1,37 @@
+from arbitration_graphs_py import Behavior
+
+
+class PrintStrings:
+    strike_through_on = "×××\010\010\010\033[9m"
+    strike_through_off = "\033[29m\033[8m×××\033[28m"
+
+    invocation_true_string = "\033[32mINVOCATION\033[39m "
+    invocation_false_string = "\033[31mInvocation\033[39m "
+    commitment_true_string = "\033[32mCOMMITMENT\033[39m "
+    commitment_false_string = "\033[31mCommitment\033[39m "
+
+
+class DummyCommandPy(str):
+    pass
+
+
+class DummyBehaviorPy(Behavior):
+    def __init__(
+        self, invocation: bool, commitment: bool, name: str = "dummy_behavior"
+    ):
+        super().__init__(name)
+        self.invocation_condition = invocation
+        self.commitment_condition = commitment
+        self.lose_control_counter = 0
+
+    def get_command(self, time):
+        return self.name
+
+    def check_invocation_condition(self, time) -> bool:
+        return self.invocation_condition
+
+    def check_commitment_condition(self, time) -> bool:
+        return self.commitment_condition
+
+    def lose_control(self, time):
+        self.lose_control_counter += 1
