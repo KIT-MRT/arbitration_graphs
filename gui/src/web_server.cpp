3f1fa330c350d09230191323c493890b2661e898
Piotr Spieker <orzechow@posteo.de>
2024-11-05 14:32:52 +0100

    Turn GUI into a header only library again

--- gui/src/web_server.cpp
+++
@@ -1,9 +0,0 @@
-/**
- * @brief Make sure to compile an object file.
- *
- * This is important for a clean install:
- * All Web App resources are installed into e.g. /usr/local/share/arbitration_graphs
- * This web root path is defined at compile time and compiled into the object file.
- */
-
-#include "gui/web_server.hpp"
