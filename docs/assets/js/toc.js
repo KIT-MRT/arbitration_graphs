15791264c226168d411085c40684fb58abef1f39
Piotr Spieker <orzechow@posteo.de>
2024-11-13 16:01:12 +0100

    Highlight active sections in table of contents

---
+++ docs/assets/js/toc.js
@@ -0,0 +1,19 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      const id = entry.target.getAttribute('id');
+      if (entry.intersectionRatio > 0) {
+        document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.add('active');
+      } else {
+        document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.remove('active');
+      }
+    });
+  });
+
+  // Track all headings
+  document.querySelectorAll('h2, h3, h4, h5, h6').forEach((section) => {
+    observer.observe(section);
+  });
+
+});
