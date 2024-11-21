95bcfb3984ec5a6dc5f1f479b06056cd27060116
Nick Le Large <nick.lelarge@kit.edu>
2024-11-21 07:22:30 +0100

    Small script tidy up
    Variable renaming to clarify which variables concern the inital version
    Remove unnecessary comments

--- .github/workflows/compute_version.sh
+++ .github/workflows/compute_version.sh
@@ -10,34 +10,30 @@ fi
 initial_version=${1//[^0-9.v]/}
 input_string=${2//[^a-zA-Z0-9#]/}
 
-# Extract the current version components
-major=$(echo "$initial_version" | cut -d'.' -f1 | cut -d'v' -f2)
-minor=$(echo "$initial_version" | cut -d'.' -f2)
-patch=$(echo "$initial_version" | cut -d'.' -f3)
+initial_major=$(echo "$initial_version" | cut -d'.' -f1 | cut -d'v' -f2)
+initial_minor=$(echo "$initial_version" | cut -d'.' -f2)
+initial_patch=$(echo "$initial_version" | cut -d'.' -f3)
 
 # Determine the bump type based on PR description
 if [[ "$input_string" == *"#major"* ]]; then
-  new_major=$((major + 1))
+  new_major=$((initial_major + 1))
   new_minor=0
   new_patch=0
 elif [[ "$input_string" == *"#minor"* ]]; then
-  new_major=$major
-  new_minor=$((minor + 1))
+  new_major=$initial_major
+  new_minor=$((initial_minor + 1))
   new_patch=0
 elif [[ "$input_string" == *"#patch"* ]]; then
-  new_major=$major
-  new_minor=$minor
-  new_patch=$((patch + 1))
+  new_major=$initial_major
+  new_minor=$initial_minor
+  new_patch=$((initial_patch + 1))
 else
   # Default to minor bump
-  new_major=$major
-  new_minor=$((minor + 1))
+  new_major=$initial_major
+  new_minor=$((initial_minor + 1))
   new_patch=0
 fi
 
-# Construct the new version
 new_version="v${new_major}.${new_minor}.${new_patch}"
-
-# Output the new version
 echo "${new_version}"
 
