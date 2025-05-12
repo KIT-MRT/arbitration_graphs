4cd24a0006cb6bf4433fd82b3b69d2d9c0bf46a9
Nick Le Large <nick.lelarge@kit.edu>
2025-05-12 12:24:50 +0000

    Make project installable via pip

--- python_bindings/src/bindings.cpp
+++ python_bindings/src/bindings.cpp
@@ -10,7 +10,7 @@
 
 namespace arbitration_graphs_py {
 
-PYBIND11_MODULE(arbitration_graphs_py, mainModule) {
+PYBIND11_MODULE(_core, mainModule) {
     bindVerificationWrapper(mainModule);
 
     bindExceptions(mainModule);
