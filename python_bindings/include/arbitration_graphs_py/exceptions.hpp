23fad1ae19ff8ed7c6ef45b61e8318fc4c4324b0
Nick Le Large <nick.lelarge@kit.edu>
2025-05-12 10:57:30 +0000

    Add exception bindings

---
+++ python_bindings/include/arbitration_graphs_py/exceptions.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <pybind11/pybind11.h>
+
+#include <arbitration_graphs/exceptions.hpp>
+
+namespace arbitration_graphs_py {
+
+namespace py = pybind11;
+using namespace arbitration_graphs;
+
+inline void bindExceptions(py::module& module) {
+    py::register_exception<GetCommandCalledWithoutGainControlError>(module, "GetCommandCalledWithoutGainControlError");
+    py::register_exception<InvocationConditionIsFalseError>(module, "InvocationConditionIsFalseError");
+    py::register_exception<MultipleReferencesToSameInstanceError>(module, "MultipleReferencesToSameInstanceError");
+    py::register_exception<InvalidArgumentsError>(module, "InvalidArgumentsError");
+    py::register_exception<VerificationError>(module, "VerificationError");
+    py::register_exception<NoApplicableOptionPassedVerificationError>(module,
+                                                                      "NoApplicableOptionPassedVerificationError");
+    py::register_exception<ApplicableOptionFailedVerificationError>(module, "ApplicableOptionFailedVerificationError");
+}
+
+} // namespace arbitration_graphs_py
