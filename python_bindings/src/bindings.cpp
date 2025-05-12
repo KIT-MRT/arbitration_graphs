e2fc1bad6348e6e28e39beb8ca87145a94333179
Nick Le Large <nick.lelarge@kit.edu>
2025-05-12 10:58:04 +0000

    Add CMakeLists and a bindings.cpp calling all the individual bindings functions

---
+++ python_bindings/src/bindings.cpp
@@ -0,0 +1,25 @@
+#include <pybind11/chrono.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+#include "arbitration_graphs_py/arbitrator.hpp"
+#include "arbitration_graphs_py/behavior.hpp"
+#include "arbitration_graphs_py/command_wrapper.hpp"
+#include "arbitration_graphs_py/exceptions.hpp"
+#include "arbitration_graphs_py/priority_arbitrator.hpp"
+#include "arbitration_graphs_py/verification_wrapper.hpp"
+
+namespace arbitration_graphs_py {
+
+PYBIND11_MODULE(arbitration_graphs_py, mainModule) {
+    bindCommandWrapper(mainModule);
+    bindVerificationWrapper(mainModule);
+
+    bindExceptions(mainModule);
+
+    bindBehavior(mainModule);
+    bindArbitrator(mainModule);
+    bindPriorityArbitrator(mainModule);
+}
+
+} // namespace arbitration_graphs_py
