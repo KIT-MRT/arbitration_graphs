41d25a5119c0f055fc44b5aa1579ca07ca2490a0
Nick Le Large <nick.lelarge@kit.edu>
2025-05-16 08:35:58 +0000

    Consistently use underscores in docker image names, a little bit of formatting

--- python_bindings/scripts/cibuildhweel.sh
+++ python_bindings/scripts/cibuildhweel.sh
@@ -1,9 +1,9 @@
 #!/bin/env bash
 
-SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 cd $SCRIPT_DIR/..
 
-docker compose build wheel-builder
+docker compose build wheel_builder
 
 # Temporary replace the symlinks with copies of the files as they can not be resolved inside the docker container
 rm version .README.md
@@ -12,7 +12,7 @@ cp ../README.md .README.md
 
 # Build the wheels
 # Currently only supporting x86_64 linux on glibc
-CIBW_SKIP="*-musllinux_*" CIBW_MANYLINUX_X86_64_IMAGE=arbitration-graphs-wheel-builder cibuildwheel --platform linux --archs x86_64
+CIBW_SKIP="*-musllinux_*" CIBW_MANYLINUX_X86_64_IMAGE=arbitration_graphs_wheel_builder cibuildwheel --platform linux --archs x86_64
 
 # Fix the symlinks again
 rm version .README.md
