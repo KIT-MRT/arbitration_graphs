5839b1a9c20925fb9a7ba4230171c9de22071c3e
Nick Le Large <nick.lelarge@kit.edu>
2025-05-21 15:22:19 +0000

    Remove setup in exception handling unit test

--- python_bindings/test/test_exceptions.py
+++ python_bindings/test/test_exceptions.py
@@ -8,53 +8,46 @@ from dummy_types import BrokenDummyBehavior, DummyBehavior
 
 
 class TestExceptionHandling(unittest.TestCase):
-    def setUp(self):
-        self.test_behavior_high_priority = BrokenDummyBehavior(
-            True, True, "HighPriority"
-        )
-        self.test_behavior_low_priority = DummyBehavior(True, True, "LowPriority")
+    def test_exception_in_priority_arbitrator(self):
+        test_behavior_high_priority = BrokenDummyBehavior(True, True, "HighPriority")
+        test_behavior_low_priority = DummyBehavior(True, True, "LowPriority")
 
-        self.time = time.time()
+        t = time.time()
 
-    def test_handle_exception(self):
         test_priority_arbitrator = ag.PriorityArbitrator()
         test_priority_arbitrator.add_option(
-            self.test_behavior_high_priority, ag.PriorityArbitrator.Option.NO_FLAGS
+            test_behavior_high_priority, ag.PriorityArbitrator.Option.NO_FLAGS
         )
         test_priority_arbitrator.add_option(
-            self.test_behavior_low_priority, ag.PriorityArbitrator.Option.NO_FLAGS
+            test_behavior_low_priority, ag.PriorityArbitrator.Option.NO_FLAGS
         )
 
-        self.assertTrue(test_priority_arbitrator.check_invocation_condition(self.time))
+        self.assertTrue(test_priority_arbitrator.check_invocation_condition(t))
 
-        test_priority_arbitrator.gain_control(self.time)
+        test_priority_arbitrator.gain_control(t)
 
         # Since the high priority behavior is broken, we should get the low priority behavior as fallback
-        self.assertEqual("LowPriority", test_priority_arbitrator.get_command(self.time))
+        self.assertEqual("LowPriority", test_priority_arbitrator.get_command(t))
         self.assertTrue(
-            test_priority_arbitrator.options()[0].verification_result.cached(self.time)
+            test_priority_arbitrator.options()[0].verification_result.cached(t)
         )
         self.assertTrue(
-            test_priority_arbitrator.options()[1].verification_result.cached(self.time)
+            test_priority_arbitrator.options()[1].verification_result.cached(t)
         )
 
         self.assertFalse(
-            test_priority_arbitrator.options()[0]
-            .verification_result.cached(self.time)
-            .is_ok()
+            test_priority_arbitrator.options()[0].verification_result.cached(t).is_ok()
         )
         self.assertTrue(
-            test_priority_arbitrator.options()[1]
-            .verification_result.cached(self.time)
-            .is_ok()
+            test_priority_arbitrator.options()[1].verification_result.cached(t).is_ok()
         )
 
-        test_priority_arbitrator.lose_control(self.time)
+        test_priority_arbitrator.lose_control(t)
 
-        self.test_behavior_low_priority.invocation_condition = False
-        self.assertTrue(test_priority_arbitrator.check_invocation_condition(self.time))
+        test_behavior_low_priority.invocation_condition = False
+        self.assertTrue(test_priority_arbitrator.check_invocation_condition(t))
 
-        test_priority_arbitrator.gain_control(self.time)
+        test_priority_arbitrator.gain_control(t)
 
         with self.assertRaises(ag.NoApplicableOptionPassedVerificationError):
-            test_priority_arbitrator.get_command(self.time)
+            test_priority_arbitrator.get_command(t)
