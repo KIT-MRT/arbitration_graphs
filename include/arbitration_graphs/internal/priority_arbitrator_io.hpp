c546958fe44d2e1af6464476a02f1577c747a616
Nick Le Large <nick.lelarge@kit.edu>
2025-05-22 08:44:13 +0000

    Update concrete arbitrator implementations to use abstract verifier

--- include/arbitration_graphs/internal/priority_arbitrator_io.hpp
+++ include/arbitration_graphs/internal/priority_arbitrator_io.hpp
@@ -10,13 +10,12 @@ namespace arbitration_graphs {
 //    PriorityArbitrator::Option    //
 //////////////////////////////////////
 
-template <typename CommandT, typename SubCommandT, typename VerifierT, typename VerificationResultT>
-std::ostream& PriorityArbitrator<CommandT, SubCommandT, VerifierT, VerificationResultT>::Option::to_stream(
-    std::ostream& output,
-    const Time& time,
-    const int& option_index,
-    const std::string& prefix,
-    const std::string& suffix) const {
+template <typename CommandT, typename SubCommandT>
+std::ostream& PriorityArbitrator<CommandT, SubCommandT>::Option::to_stream(std::ostream& output,
+                                                                           const Time& time,
+                                                                           const int& option_index,
+                                                                           const std::string& prefix,
+                                                                           const std::string& suffix) const {
 
     output << option_index + 1 << ". ";
     ArbitratorBase::Option::to_stream(output, time, option_index, prefix, suffix);
@@ -28,8 +27,8 @@ std::ostream& PriorityArbitrator<CommandT, SubCommandT, VerifierT, VerificationR
 //        PriorityArbitrator        //
 //////////////////////////////////////
 
-template <typename CommandT, typename SubCommandT, typename VerifierT, typename VerificationResultT>
-YAML::Node PriorityArbitrator<CommandT, SubCommandT, VerifierT, VerificationResultT>::toYaml(const Time& time) const {
+template <typename CommandT, typename SubCommandT>
+YAML::Node PriorityArbitrator<CommandT, SubCommandT>::toYaml(const Time& time) const {
     YAML::Node node = ArbitratorBase::toYaml(time);
     node["type"] = "PriorityArbitrator";
     return node;
