1b3780ef132264fa4b2b562b699e8dba603dc0ad
Nick Le Large <nick.lelarge@kit.edu>
2025-06-27 13:08:20 +0000

    Fix verifier bindings using pybind11v3 features
    This fixes the issues described here
    https://pybind11.readthedocs.io/en/latest/advanced/classes.html#avoiding-inheritance-slicing-and-std-weak-ptr-surprises
    and here by moving pybind to the v3 (currently pre-release) allowing to
    use the smart_holder and trampoline_self_life_support features.

--- python_bindings/include/arbitration_graphs_py/verifier.hpp
+++ python_bindings/include/arbitration_graphs_py/verifier.hpp
@@ -19,7 +19,7 @@ namespace agv = ag::verification;
 
 
 /// @brief A wrapper class (a.k.a. trampoline class) for the AbstractResult class to allow Python overrides.
-class PyAbstractResult : public agv::AbstractResult {
+class PyAbstractResult : public agv::AbstractResult, py::trampoline_self_life_support {
 public:
     // NOLINTBEGIN(readability-function-size)
     bool isOk() const override {
@@ -28,7 +28,7 @@ public:
     // NOLINTEND(readability-function-size)
 };
 
-class PyAbstractVerifier : public agv::AbstractVerifier<CommandWrapper> {
+class PyAbstractVerifier : public agv::AbstractVerifier<CommandWrapper>, py::trampoline_self_life_support {
 public:
     using AbstractVerifierT = agv::AbstractVerifier<CommandWrapper>;
 
@@ -43,20 +43,19 @@ inline void bindVerifier(py::module& module) {
     using AbstractVerifierT = agv::AbstractVerifier<CommandWrapper>;
     using PlaceboVerifierT = agv::PlaceboVerifier<CommandWrapper>;
 
-    py::class_<agv::AbstractResult, PyAbstractResult, std::shared_ptr<agv::AbstractResult>>(module, "AbstractResult")
+    py::class_<agv::AbstractResult, PyAbstractResult, py::smart_holder>(module, "AbstractResult")
         .def(py::init<>())
         .def("is_ok", &agv::AbstractResult::isOk);
 
-    py::class_<agv::PlaceboResult, agv::AbstractResult, std::shared_ptr<agv::PlaceboResult>>(module, "PlaceboResult")
+    py::class_<agv::PlaceboResult, agv::AbstractResult, py::smart_holder>(module, "PlaceboResult")
         .def(py::init<bool>(), py::arg("is_ok") = true)
         .def("is_ok", &agv::PlaceboResult::isOk);
 
-    py::class_<AbstractVerifierT, PyAbstractVerifier, std::shared_ptr<AbstractVerifierT>>(module, "AbstractVerifier")
+    py::class_<AbstractVerifierT, PyAbstractVerifier, py::smart_holder>(module, "AbstractVerifier")
         .def(py::init<>())
         .def("analyze", &AbstractVerifierT::analyze);
 
-    py::class_<PlaceboVerifierT, AbstractVerifierT, std::shared_ptr<PlaceboVerifierT>>(module, "PlaceboVerifier")
-        .def(py::init<>());
+    py::class_<PlaceboVerifierT, AbstractVerifierT, py::smart_holder>(module, "PlaceboVerifier").def(py::init<>());
 }
 
 } // namespace arbitration_graphs_py
