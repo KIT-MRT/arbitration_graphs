8c78d04b6e9fb1c5dbde170c085123410d6835cf
Nick Le Large <nick.lelarge@kit.edu>
2025-07-04 21:04:10 +0200

    Replace relative image paths with absolute links during preprocessing

--- python_bindings/scripts/cibuildwheel.sh
+++ python_bindings/scripts/cibuildwheel.sh
@@ -5,14 +5,18 @@ cd $SCRIPT_DIR/..
 
 docker compose build --pull wheel_builder
 
-# Temporary replace the symlinked version with a copy as the link can not be resolved inside the docker container
+# Temporarily replace the symlinked version with a copy as the link can not be resolved inside the docker container
 rm version
 cp ../version .
+
+# Temporarily replace the relative image paths in the README with absolute URLs to be able to display them on PyPI
+sed -i 's|\.\./docs/assets|https://raw.githubusercontent.com/KIT-MRT/arbitration_graphs/main/docs/assets|g' README.md
 
 # Build the wheels
 # Currently only supporting x86_64 linux on glibc
 CIBW_SKIP="*-musllinux_*" CIBW_MANYLINUX_X86_64_IMAGE=arbitration_graphs_wheel_builder cibuildwheel --platform linux --archs x86_64
 
-# Fix the symlink again
+# Be a good citizen and undo the changes above
 rm version
 ln -s ../version .
+sed -i 's|https://raw.githubusercontent.com/KIT-MRT/arbitration_graphs/main/docs/assets|\.\./docs/assets|g' README.md
