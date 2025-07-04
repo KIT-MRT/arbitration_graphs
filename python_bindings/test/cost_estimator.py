a010bcbb8fab264e1648beb877ad1a2e68847be1
Nick Le Large <nick.lelarge@kit.edu>
2025-07-04 20:16:24 +0000

    Adjust cost estimator to new interface

--- python_bindings/test/cost_estimator.py
+++ python_bindings/test/cost_estimator.py
@@ -7,7 +7,11 @@ class CostEstimatorFromCostMap(CostEstimator):
         self.cost_map = cost_map
         self.activation_costs = activation_costs
 
-    def estimate_cost(self, command, is_active):
+    def estimate_cost(self, time, environment_model, command, is_active):
+        # In an actual application, you might want to get data from the environment model here to help you determine
+        # the cost of a command. In this mock implementation, we simply simulate this access to show that we could do
+        # so.
+        environment_model.get_observation()
         if is_active:
             return self.cost_map[command] / (1 + self.activation_costs)
         else:
