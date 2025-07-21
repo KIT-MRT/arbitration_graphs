b49cf3172540a8b02f5e8953519e3c42367f71ff
Nick Le Large <nick.lelarge@kit.edu>
2025-07-21 15:50:16 +0000

    Add typing annotations in python implemented classes

--- python_bindings/test/dummy_types.py
+++ python_bindings/test/dummy_types.py
@@ -1,23 +1,26 @@
+from typing_extensions import override
+
 from arbitration_graphs import Behavior
+from arbitration_graphs.typing import Time
 from arbitration_graphs.verification import Result, Verifier
 
 
 class PrintStrings:
-    strike_through_on = "×××\010\010\010\033[9m"
-    strike_through_off = "\033[29m\033[8m×××\033[28m"
+    strike_through_on: str = "×××\010\010\010\033[9m"
+    strike_through_off: str = "\033[29m\033[8m×××\033[28m"
 
-    invocation_true = "\033[32mINVOCATION\033[39m "
-    invocation_false = "\033[31mInvocation\033[39m "
-    commitment_true = "\033[32mCOMMITMENT\033[39m "
-    commitment_false = "\033[31mCommitment\033[39m "
+    invocation_true: str = "\033[32mINVOCATION\033[39m "
+    invocation_false: str = "\033[31mInvocation\033[39m "
+    commitment_true: str = "\033[32mCOMMITMENT\033[39m "
+    commitment_false: str = "\033[31mCommitment\033[39m "
 
 
 class DummyEnvironmentModel:
     def __init__(self):
-        self.access_counter = 0
+        self.access_counter: int = 0
 
     # A mock getter function that simulates accessing the environment model.
-    def get_observation(self):
+    def get_observation(self) -> None:
         self.access_counter += 1
 
 
@@ -30,29 +33,44 @@ class DummyBehavior(Behavior):
         self, invocation: bool, commitment: bool, name: str = "dummy_behavior"
     ):
         super().__init__(name)
-        self.invocation_condition = invocation
-        self.commitment_condition = commitment
-        self.lose_control_counter = 0
+        self.invocation_condition: bool = invocation
+        self.commitment_condition: bool = commitment
+        self.lose_control_counter: int = 0
 
-    def get_command(self, time, environment_model):
+    @override
+    def get_command(
+        self, time: Time, environment_model: DummyEnvironmentModel
+    ) -> DummyCommand:
         # While computing a command, we can read the environment model to get the required context. In this mock
         # implementation, we merely simulate this access to assure that this interaction is possible. You will find
         # analogous calls to the environment model in the other methods of this class.
         environment_model.get_observation()
-        return self.name()
+        return DummyCommand(self.name())
 
-    def check_invocation_condition(self, time, environment_model) -> bool:
+    @override
+    def check_invocation_condition(
+        self, time: Time, environment_model: DummyEnvironmentModel
+    ) -> bool:
         environment_model.get_observation()
         return self.invocation_condition
 
-    def check_commitment_condition(self, time, environment_model) -> bool:
+    @override
+    def check_commitment_condition(
+        self, time: Time, environment_model: DummyEnvironmentModel
+    ) -> bool:
         environment_model.get_observation()
         return self.commitment_condition
 
-    def gain_control(self, time, environment_model):
+    @override
+    def gain_control(
+        self, time: Time, environment_model: DummyEnvironmentModel
+    ) -> None:
         environment_model.get_observation()
 
-    def lose_control(self, time, environment_model):
+    @override
+    def lose_control(
+        self, time: Time, environment_model: DummyEnvironmentModel
+    ) -> None:
         environment_model.get_observation()
         self.lose_control_counter += 1
 
@@ -66,37 +84,48 @@ class BrokenDummyBehavior(DummyBehavior):
         num_get_commands_until_raise: int = 0,
     ):
         super().__init__(invocation, commitment, name)
-        self.num_get_commands_until_raise = num_get_commands_until_raise
-        self.get_command_counter = 0
+        self.num_get_commands_until_raise: int = num_get_commands_until_raise
+        self.get_command_counter: int = 0
 
-    def get_command(self, time, environment_model):
+    @override
+    def get_command(
+        self, time: Time, environment_model: DummyEnvironmentModel
+    ) -> DummyCommand:
         if self.get_command_counter >= self.num_get_commands_until_raise:
             raise Exception("BrokenDummyBehavior::getCommand() is broken")
         environment_model.get_observation()
 
         self.get_command_counter += 1
-        return self.name()
+        return DummyCommand(self.name())
 
 
 class DummyResult(Result):
-    def __init__(self, is_ok=True):
+    def __init__(self, is_ok: bool = True):
         Result.__init__(self)
-        self._is_ok = is_ok
+        self._is_ok: bool = is_ok
 
-    def is_ok(self):
+    @override
+    def is_ok(self) -> bool:
         return self._is_ok
 
+    @override
     def __str__(self):
         return "is okay" if self.is_ok() else "is not okay"
 
 
 class DummyVerifier(Verifier):
-    def __init__(self, wrong="", reject_all=False):
+    def __init__(self, wrong: str = "", reject_all: bool = False):
         Verifier.__init__(self)
-        self.wrong = wrong
-        self.reject_all = reject_all
+        self.wrong: str = wrong
+        self.reject_all: bool = reject_all
 
-    def analyze(self, time, environment_model, command):
+    @override
+    def analyze(
+        self,
+        time: Time,
+        environment_model: DummyEnvironmentModel,
+        command: DummyCommand,
+    ) -> DummyResult:
         if self.reject_all or command == self.wrong:
             return DummyResult(False)
 
