667d599f72fc27417e06c812d1535d64fd24e341
Nick Le Large <nick.lelarge@kit.edu>
2025-07-21 12:11:26 +0000

    Replace wild card import and sys.modules hack with extensive list of modules

---
+++ python_bindings/src/arbitration_graphs/verification/__init__.py
@@ -0,0 +1,13 @@
+from arbitration_graphs.arbitration_graphs_py.verification import (
+    PlaceboVerifier,
+    Result,
+    SimpleResult,
+    Verifier,
+)
+
+__all__ = [
+    "PlaceboVerifier",
+    "Result",
+    "SimpleResult",
+    "Verifier",
+]
