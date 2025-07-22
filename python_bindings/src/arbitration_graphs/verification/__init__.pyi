63347e90241a0da1be7626cec55abb3e1bcceb8b
Nick Le Large <nick.lelarge@kit.edu>
2025-07-22 09:38:37 +0000

    Be explicit about overrides in stub files as well

--- python_bindings/src/arbitration_graphs/verification/__init__.pyi
+++ python_bindings/src/arbitration_graphs/verification/__init__.pyi
@@ -1,5 +1,7 @@
 # pyright: reportAny=false,reportExplicitAny=false
 
+from typing_extensions import override
+
 from arbitration_graphs.typing import Command, EnvironmentModel, Time
 
 __all__ = ["PlaceboVerifier", "Result", "SimpleResult", "Verifier"]
@@ -13,6 +15,7 @@ class Result:
 
 class SimpleResult(Result):
     def __init__(self, is_ok: bool) -> None: ...
+    @override
     def is_ok(self) -> bool: ...
 
 class Verifier:
