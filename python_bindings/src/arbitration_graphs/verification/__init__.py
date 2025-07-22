f181bbc993556479fa0f09acb93f5033e02499af
Nick Le Large <nick.lelarge@kit.edu>
2025-07-22 09:44:25 +0000

    Suppress some warnings that cannot be resolved

--- python_bindings/src/arbitration_graphs/verification/__init__.py
+++ python_bindings/src/arbitration_graphs/verification/__init__.py
@@ -1,3 +1,7 @@
+# These warnings are suppressed because Pyright cannot resolve symbols inside
+# the pybind11-generated shared library that will be loaded at runtime.
+# pyright: reportMissingImports=false,reportUnknownVariableType=false
+
 from arbitration_graphs.arbitration_graphs_py.verification import (
     PlaceboVerifier,
     Result,
